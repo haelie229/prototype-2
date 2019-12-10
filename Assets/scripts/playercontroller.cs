ff6352f02a2c1567713421b50ec5cbc9cfee751c
haelie229 <56128971+haelie229@users.noreply.github.com>
2019-12-10 10:01:05 -0800

    changed cods and made player moe

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -9,30 +9,38 @@ public class playercontroller : MonoBehaviour
     public float xRange = 10;
 
     public GameObject projectilePrefab;
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     // keep the player in bounds
-    { 
-    if (transform.position.x < -xRange)
     {
-        transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-    }
-    if (transform.position.x > xRange)
-    {    
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+
+
+        }
         horizontalinput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalinput * Time.deltaTime * speed);
-    } transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        
-        
+        }
+    }
+} 
+
+
+
 
